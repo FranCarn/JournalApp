a321d83307e3b4bab8896634596f250700bbbcc8
FranCarn <carnevalefranco99@gmail.com>
2022-11-15 00:30:37 -0300

    fix upload

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -21,10 +21,6 @@ export const AppRouter = () => {
         <Route path="/auth/*" element={<AuthRoutes />} />
       )}
       <Route path="/*" element={<Navigate to="/auth/login" />} />
-
-      {/* <Route path="/auth/*" element={<AuthRoutes />} />
-
-       <Route path="/*" element={<JournalRoutes />} /> */}
     </Routes>
   );
 };
